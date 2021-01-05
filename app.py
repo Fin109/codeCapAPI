ae2cb8f650c17980c5a4db7c9187b2ab6caf3043
Fin109 <53404262+Fin109@users.noreply.github.com>
2021-01-05 12:41:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify # Import Flask and jsonify to return JSON info
 import requests
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-
+@app.route('/')
 def index():
+	return "Welcome to my API!"
+
+@app.route('/get',methods=['GET'])
+def get():
 	FIXER_URL = 'https://openexchangerates.org/api/latest.json?app_id=b833cea4b2854e46ad377d019eeba7da'
 	FIXER_PARAMS = {'symbols':'ZAR,EUR,CAD'}
 	fixer = requests.get(FIXER_URL,params=FIXER_PARAMS)  # <---- PINGS API, gets data
